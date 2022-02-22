f50aaa72034c376c8f9809fc2f8f6bc7cab5507f
xiaxiaotong998 <xiaotong.xia4@gmail.com>
2022-02-22 11:57:51 +0800

    success

--- src/main/java/com/example/demo/Indexcontroller.java
+++ src/main/java/com/example/demo/Indexcontroller.java
@@ -1,51 +1,56 @@
 package com.example.demo;
 
-import java.sql.*;
-import java.util.List;
-
 import com.azure.core.http.rest.PagedIterable;
-import com.example.demo.user;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.azure.spring.utils.AzureCloudUrls;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.azure.storage.blob.*;
-import com.azure.storage.blob.models.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Indexcontroller {
 
     String connectStr = "DefaultEndpointsProtocol=https;AccountName=testexample;AccountKey=gtjq2b6hqIiIDNlwPXYowDWraSfJ7v7eVPuveDKKzaHzmOmDr/ivdRYHx/ikF1h64bTtC5e/GgTW+AStRGUm1A==;EndpointSuffix=core.windows.net";
-    String connectionUrl = "jdbc:sqlserver://testexample.database.windows.net:1433;database=testexample;user=Xiaxiaotong@testexample;password=Xiaxt35363536.;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-    @GetMapping("/pic")
-    public String index() throws SQLException {
+    String connectionUrl = "jdbc:sqlserver://testexample.database.windows.net:1433;database=testexample;User=Xiaxiaotong@testexample;password=Xiaxt35363536.;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
+    @GetMapping("/userName")
+    public String index() throws SQLException {
         Connection connection = DriverManager.getConnection(connectionUrl);
         Statement statement = connection.createStatement();
-
         String selectSql = "SELECT * from [User]";
         ResultSet resultSet = statement.executeQuery(selectSql);
-
-
-
         while (resultSet.next()) {
             return "Hello Azure Service, " + resultSet.getString(2);
         }
-
-
         return null;
     }
 
+    @GetMapping("/pic")
+    public List<String> pic() {
+        ArrayList<String> list = new ArrayList<>();
 
-    @GetMapping("")
-    public PagedIterable<BlobItem> index1() {
-
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
-        String containerName = "testexampleblobs2e6b5347-37eb-4ce4-9f9a-46e1c445426d";
-        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
-
-//        for (BlobItem blobItem : containerClient.listBlobs()) {
-//            return "Blob name: " + blobItem.getName() + ", Snapshot: " + "https://testexample.blob.core.windows.net/testexampleblobs2e6b5347-37eb-4ce4-9f9a-46e1c445426d/screenshot-1.png?snapshot=2022-02-16T09:16:08.9155424Z&sv=2020-08-04&ss=bqtf&srt=sco&sp=rwdlacuptfxi&se=2022-02-21T15:20:38Z&sig=L39sjU8SwLhBxdJr8rzONf5thJkBzCnOik2IKO2x0f4%3D&_=1645439177315";
-//        }
-       return containerClient.listBlobs();
+        //BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
+        //String containerName = "testexampleblobs2e6b5347-37eb-4ce4-9f9a-46e1c445426d";
+        //BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+        //PagedIterable<BlobItem> blobItems = containerClient.listBlobs();
+        //for (BlobItem blobItem : blobItems) {
+        //    BlobClient blobClient =
+        //            containerClient.getBlobClient(blobItem.getName());
+        //    //list.add(blobClient.getBlobUrl());
+        //}
+        list.add("testexample.blob.core.windows.net/testexampleblobs2e6b5347-37eb-4ce4-9f9a-46e1c445426d/pic/screenshot-1.png");
+        list.add("testexample.blob.core.windows" +
+                ".net/testexampleblobs2e6b5347-37eb-4ce4-9f9a-46e1c445426d/pic/screenshot-2.png");
+        list.add("testexample.blob.core.windows" +
+                ".net/testexampleblobs2e6b5347-37eb-4ce4-9f9a-46e1c445426d/pic/screenshot-3.png");
+        return list;
     }
+
 }
